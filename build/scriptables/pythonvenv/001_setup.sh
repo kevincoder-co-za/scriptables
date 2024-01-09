d11d47d08b17204b9286820be1286a54ccefe6bf
Kevin Naidoo <kevin.naidoo@pricecheck.co.za>
2024-01-09 08:36:39 +0200

    Adds support for setting up python virtual env

---
+++ build/scriptables/pythonvenv/001_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# exit-on-failure=yes
+
+sudo apt install python3-venv python3-dev libpq-dev -y
+python3 -m venv ~/.venv
+
+/.venv/bin/pip3 install gunicorn
+/.venv/bin/pip3 install uvicorn
